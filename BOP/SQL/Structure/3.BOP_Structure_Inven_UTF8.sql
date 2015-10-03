3bd92289e18a9c35f4265b2f4a2803f6085fdf65
surachai-tw <surachai.tw@gmail.com>
2015-10-03 23:42:33 +0700

    เพิ่ม table bpk_stock_mgnt_his_working

--- BOP/SQL/Structure/3.BOP_Structure_Inven_UTF8.sql
+++ BOP/SQL/Structure/3.BOP_Structure_Inven_UTF8.sql
@@ -1,3 +1,24 @@
+CREATE TABLE bpk_stock_mgnt_his_working 
+(
+    begin_date VARCHAR(255) NOT NULL,  
+    id SERIAL PRIMARY KEY, 
+    stock_mgnt_id VARCHAR(255) DEFAULT '', 
+    item_id VARCHAR(255) DEFAULT '', 
+    stock_id VARCHAR(255) DEFAULT '', 
+    lot_number VARCHAR(255) DEFAULT '', 
+    lot_number_id VARCHAR(255) DEFAULT '', 
+    quantity FLOAT DEFAULT 0, 
+    small_unit_id VARCHAR(255) DEFAULT '', 
+    unit_lot_cost FLOAT DEFAULT 0, 
+    unit_avg_cost FLOAT DEFAULT 0
+);
+CREATE INDEX idx_stock_mgnt_his_wk_begin_date ON bpk_stock_mgnt_his_working(begin_date);
+CREATE INDEX idx_stock_mgnt_his_wk_stock_mgnt_id ON bpk_stock_mgnt_his_working(stock_mgnt_id);
+CREATE INDEX idx_stock_mgnt_his_wk_item_id ON bpk_stock_mgnt_his_working(item_id);
+CREATE INDEX idx_stock_mgnt_his_wk_wk_stock_id ON bpk_stock_mgnt_his_working(stock_id);
+CREATE INDEX idx_stock_mgnt_his_wk_lot_number ON bpk_stock_mgnt_his_working(lot_number);
+CREATE INDEX idx_stock_mgnt_his_wk_lot_number_id ON bpk_stock_mgnt_his_working(lot_number_id);
+
 DROP TABLE bpk_stock_mgnt_his;
 -- For snapshot data inventory at 00:00 
 CREATE TABLE bpk_stock_mgnt_his 
