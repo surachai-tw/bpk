dfb67eaa857a8838f8fcb7a2fa9ce2b112a5024f
surachai-tw <surachai.tw@gmail.com>
2016-03-14 23:34:31 +0700

    ทดสอบ Barcode Reader

--- BarCodeReader/src/RunScanWithThreshold.java
+++ BarCodeReader/src/RunScanWithThreshold.java
@@ -1,9 +1,11 @@
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import com.bytescout.barcodereader.BarcodeType;
 import com.bytescout.barcodereader.FoundBarcode;
 import com.bytescout.barcodereader.Reader;
+import java.awt.Graphics2D;
 import java.util.EnumSet;
 
 /**
@@ -12,22 +14,26 @@ import java.util.EnumSet;
  */
 public class RunScanWithThreshold
 {
+
     public static void main(String args[])
     {
         try
         {
-            System.out.println("Original, TEXT = "+getTextFromBarcode("D:\\BPK\\trunk\\BarCodeReader\\file\\sample\\SCAN.jpg"));
+            System.out.println("Original, TEXT = " + getTextFromBarcode("D:\\BPKHIS\\trunk\\Sources\\CodeJSP\\jboss\\server\\default\\deploy\\bpkimage.war\\backup\\bak\\TEST_SYMBOL.jpg"));
+
+            /*
             for (int i=100; i>=1; i--)
             {
-                BufferedImage img = ImageIO.read(new File("D:\\BPK\\trunk\\BarCodeReader\\file\\sample\\SCAN.jpg"));
-                ImageTool.toBlackAndWhite(img, i);
-                String newname = "D:\\BPK\\trunk\\BarCodeReader\\file\\sample\\CHECK_" + i + ".jpg";
-                ImageIO.write(img, "jpg", new File(newname));
+            BufferedImage img = ImageIO.read(new File("D:\\BPK\\trunk\\BarCodeReader\\file\\sample\\SCAN.jpg"));
+            ImageTool.toBlackAndWhite(img, i);
+            String newname = "D:\\BPK\\trunk\\BarCodeReader\\file\\sample\\CHECK_" + i + ".jpg";
+            ImageIO.write(img, "jpg", new File(newname));
 
-                System.out.println("Precision = "+i+", TEXT = "+getTextFromBarcode(newname));
+            System.out.println("Precision = "+i+", TEXT = "+getTextFromBarcode(newname));
             }
-        }
-        catch(Exception ex)
+             * 
+             */
+        } catch (Exception ex)
         {
             ex.printStackTrace();
         }
@@ -46,6 +52,16 @@ public class RunScanWithThreshold
 
             // Demonstrate barcode decoding from image file:
             FoundBarcode[] foundBarcodes = reader.readFromFile(filename);
+            /*
+            BufferedImage in = ImageIO.read(new File(filename));
+            BufferedImage newImage = new BufferedImage(
+                    in.getWidth(), in.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = newImage.createGraphics();
+            g.drawImage(in, 0, 0, null);
+            g.dispose();
+            FoundBarcode[] foundBarcodes = reader.readFromImage(newImage);
+             *
+             */
             if (foundBarcodes.length > 0)
             {
                 for (int i = 0; i < foundBarcodes.length; i++)
@@ -66,16 +82,12 @@ public class RunScanWithThreshold
                      */
                 }
             }
-        }
-        catch (Exception ex)
+        } catch (Exception ex)
         {
             ex.printStackTrace();
-        }
-        finally
+        } finally
         {
         }
         return result;
     }
-
-
 }
